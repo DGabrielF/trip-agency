50f23ed01feb8f919d459647f66249722d199d1a
gabriel <dgabrielfof@gmail.com>
2024-09-18 14:25:59 -0300

    :sparkles: feat: fill in some initial data to start building the site

---
+++ src/scripts/data.js
@@ -0,0 +1,234 @@
+export const data = {
+  company: {
+    name: "Rota dos Sonhos",
+    logo: "",
+    details: {
+      foundation: "16/09/2024",
+      openingHours: [
+        {
+          id: 0,
+          day: "Domingo",
+          shortDay: "Dom",
+          start: "",
+          end: "",
+        },
+        {
+          id: 1,
+          day: "Segunda",
+          shortDay: "Seg",
+          start: "8:00",
+          end: "18:00",
+        },
+        {
+          id: 2,
+          day: "Terça",
+          shortDay: "Ter",
+          start: "8:00",
+          end: "18:00",
+        },
+        {
+          id: 3,
+          day: "Quarta",
+          shortDay: "Qua",
+          start: "8:00",
+          end: "18:00",
+        },
+        {
+  
+          id: 4,
+          day: "Quinta",
+          shortDay: "Qui",
+          start: "8:00",
+          end: "18:00",
+        },
+        {
+          id: 5,
+          day: "Sexta",
+          shortDay: "Sex",
+          start: "8:00",
+          end: "18:00",
+        },
+        {
+          id: 6,
+          day: "Sábado",
+          shortDay: "Sáb",
+          start: "",
+          end: "",
+        }
+      ],
+    },
+    contact: {
+      address: {},
+      phone: {},
+      whatsapp: {},
+      instagram: {},
+    },
+    team: [
+      {
+        name: "Danilo Ferreira",
+        images: [
+          {
+            src: "",
+            alt: "",
+          }
+        ],
+        contact: {
+          whatsapp: {},
+          instagram: {},
+        }
+      },
+      {
+        name: "Fayla Diamantino",
+        images: [
+          {
+            src: "",
+            alt: "",
+          }
+        ],
+        contact: {
+          whatsapp: {},
+          instagram: {},
+        }
+      },
+    ]
+  },
+  pages: {
+    banner: {
+      title: "",
+      subtitle: "",
+      description: [""],
+      imagens: [
+        {
+          src: "",
+          alt: "",
+        }
+      ],
+      items: [],
+    },
+    menu: {
+      title: "",
+      subtitle: "",
+      description: [""],
+      imagens: [
+        {
+          src: "",
+          alt: "",
+        }
+      ],
+      items: [
+        {
+          text: "Já programadas",
+          key: "schedule",
+          options: []
+        },
+        {
+          text: "Destinos",
+          key: "destination",
+          options: [
+            {
+              text: "Escolher",
+              key: "choose",
+              status: null,
+            },
+            {
+              text: "Match",
+              key: "match",
+              status: null,
+            },
+            {
+              text: "Surpresa",
+              key: "random",
+              status: null,
+            },
+          ]
+        },
+        {
+          text: "Hospedagens",
+          key: "hosters",
+          options: []
+        },
+        {
+          text: "Usuário",
+          key: "user",
+          options: [
+            {
+              text: "Entrar",
+              key: "signin",
+              status: false,
+            },
+            {
+              text: "Registrar",
+              key: "signup",
+              status: false,
+            },
+            {
+              text: "Perfil",
+              key: "profile",
+              status: true,
+            },
+            {
+              text: "Sair",
+              key: "signout",
+              status: true,
+            }
+          ]
+        },
+      ],
+    },
+    schedule: {
+      title: "Veja nossos pacotes prontos",
+      subtitle: "A um toque de uma experiência maravilhante",
+      description: [
+        "Nosso cronograma é montado com as viagens mais populares do nosso menu.",
+        "Se está aparecendo aqui, é porque ainda dá pra você!",
+        "Tudo já está devidamente planejado e organizado pra você só curtir."
+      ],
+      imagens: [
+        {
+          src: "",
+          alt: "",
+        }
+      ],
+      items: [],
+    },
+    destination: {
+      title: "Escolha o seu destino",
+      subtitle: "",
+      description: [""],
+      imagens: [
+        {
+          src: "",
+          alt: "",
+        }
+      ],
+      items: [],
+    },
+    quizz: {
+      title: "Que tal um quiz pra tirar essa dúvida?",
+      subtitle: "",
+      description: [
+        "Sabe quando a gente quer viajar, mas aparece um monte de coisa pra escolher...",
+        "Bem, separamos algumas perguntas, pra tentar te ajudar a poupar seu tempo e filtrar opções para você!"
+      ],
+      imagens: [
+        {
+          src: "",
+          alt: "",
+        }
+      ],
+      items: [],
+    },
+    hosters: {
+      title: "Hospedagens",
+      subtitle: "Lugares pra chamar de lar, estando longe do seu lar",
+      description: [""],
+      imagens: [
+        {
+          src: "",
+          alt: "",
+        }
+      ],
+      items: [],
+    },
+  }
+}
