fc2962543dac51bb90594917c4a5dc72d0d4fcf4
gabriel <dgabrielfof@gmail.com>
2024-09-18 14:35:50 -0300

    :sparkles: feat: structuring the layout of the sections that will be injected into the site with their basic functions

---
+++ src/scripts/main.js
@@ -0,0 +1,34 @@
+function init () {
+  // Banner
+  // // Logo
+  // // Carrossel de imagens
+  // Menu
+  // // Login
+  // // Registro
+  // // Perfil
+  // // Logout
+  // Schedule
+  // // Título
+  // // Carrossel de cartões
+  // Destination
+  // // Título
+  // // Carrossel de cartões
+  // Hosters
+  // // Título
+  // // Carrossel de cartões
+  // Tours
+  // // Título
+  // // Carrossel de cartões
+  // Quizz
+  // // Título
+  // // Carrossel de cartões
+  // Form
+  // // Apresentação das perguntas
+  // // Validação das respostas
+  // Budget
+  // // Apresentação do orçamento
+  // // Mostrar e Esconder
+  // // Detalhamento do orçamento
+}
+
+init ();
